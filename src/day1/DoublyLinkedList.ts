d435b2068060057011224ee58f8b8da741dda054
reistence <gvlamberti@gmail.com>
2023-10-16 10:20:49 +0200

    doublylinkedlist set up

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,30 @@
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    public head: null | Node<T> = null;
+    public tail: null | Node<T> = null;
 
     constructor() {
+        this.length = 0;
     }
 
-    prepend(item: T): void {
+    prepend(item: T): void {}
 
-}
-    insertAt(item: T, idx: number): void {
+    insertAt(item: T, idx: number): void {}
 
-}
-    append(item: T): void {
+    append(item: T): void {}
 
-}
-    remove(item: T): T | undefined {
+    remove(item: T): T | undefined {}
 
-}
-    get(idx: number): T | undefined {
+    get(idx: number): T | undefined {}
 
+    removeAt(idx: number): T | undefined {}
 }
-    removeAt(idx: number): T | undefined {
 
-}
+class Node<T> {
+    public value: T;
+    public next: Node<T> | null;
+    constructor(value: T) {
+        this.value = value;
+        this.next = null;
+    }
 }
