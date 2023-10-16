5db689879e615de58d7c9d4c0dc0273cd8e7adb9
reistence <gvlamberti@gmail.com>
2023-10-16 11:51:28 +0200

    doublylinkedlist almost all methods

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -9,20 +9,115 @@ export default class DoublyLinkedList<T> {
 
     prepend(item: T): void {
         let node = new Node(item);
-
         if (this.head === null) {
+            this.head = node;
+        } else {
+            let nextNode = this.head;
+            node.next = nextNode;
+            nextNode.prev = node;
+            this.head = node;
+        }
+        this.length++;
+        return;
+    }
+
+    append(item: T): void {
+        let node = new Node(item);
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            if (current != null) {
+                while (current?.next) {
+                    current = current.next;
+                }
+
+                current.next = node;
+                node.prev = current;
+            }
+        }
+        this.length++;
+    }
+
+    remove(item: T): T | undefined {
+        let current = this.head;
+        let previousNode = null;
+        if (current?.value === item) {
+            this.head = current.next;
+            this.length--;
+            return current.value;
+        } else {
+            while (current) {
+                if (current.value === item) {
+                    if (current.prev) {
+                        current.prev.next = current.next;
+                    }
+                    if (current.next) {
+                        current.next.prev = current.prev;
+                    }
+                    this.length--;
+                    return current.value;
+                }
+                current = current.next;
+            }
+        }
+        return undefined;
+    }
+
+    get(idx: number): T | undefined {
+        let i = 0;
+        let current = this.head;
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
+        while (i < idx) {
+            if (current) {
+                current = current.next;
+            }
+            i++;
+        }
+        if (current) {
+            return current.value;
+        }
+        return undefined;
+    }
+
+    insertAt(item: T, idx: number): void {
+        let i = 0;
+        let current = this.head;
+        let node = new Node(item);
+        let previousNode = null;
+
+        if (idx < 0 || idx >= this.length) {
+            return;
+        }
+        if (idx === 0) {
+            this.prepend(item);
+            return;
+        } else {
+            while (i < idx) {
+                if (current) {
+                    previousNode = current;
+                    current = current.next;
+                }
+                i++;
+            }
+            node.next = current;
+            if (current) {
+                current.prev = node;
+            }
+            if (previousNode) {
+                previousNode.next = node;
+                node.prev = previousNode;
+            }
+            this.length++;
+            return;
         }
     }
 
-    append(item: T): void {}
-
-    remove(item: T): T | undefined {}
-
-    get(idx: number): T | undefined {}
-
-    insertAt(item: T, idx: number): void {}
-
-    removeAt(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {
+        return undefined;
+    }
 }
 
 class Node<T> {
