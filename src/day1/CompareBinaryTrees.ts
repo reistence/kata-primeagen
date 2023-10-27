da09e219f31d5301b6c91e3bf2689ebf8d58a874
reistence <gvlamberti@gmail.com>
2023-10-27 16:17:49 +0200

    Binary trees comparation

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,23 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+// Deep search preserve SHAPE
 
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    //base cases
+    if (a === null && b === null) {
+        return true;
+    }
+
+    if (a === null || b === null) {
+        return false;
+    }
+
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    // recursion
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
