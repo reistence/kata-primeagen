c4ae2890a2063c042eb80fc5e0bbbbebbb3d90ce
reistence <gvlamberti@gmail.com>
2023-10-27 15:38:35 +0200

    binary trees traversing

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,18 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
+    //recursion
+    //pre
+    //recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    //post
+    path.push(curr.value);
+    // no post ops since is preorder
+    return path;
+}
 
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,18 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
+    //recursion
+    //pre
+    path.push(curr.value);
+    //recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    //post
+    // no post ops since is preorder
+    return path;
+}
 
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
