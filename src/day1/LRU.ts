b78963c617151a6cc84385712ebe052896585e8c
reistence <gvlamberti@gmail.com>
2023-11-19 16:56:05 +0100

    LRU implemented

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,106 @@
+// Last Recently Used
+
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
+function createNode<V>(value: V): Node<V> {
+    return { value };
+}
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookUp: Map<K, Node<V>>;
+    private reverseLookUp: Map<Node<V>, K>;
 
-    constructor() {
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookUp = new Map<K, Node<V>>();
+        this.reverseLookUp = new Map<Node<V>, K>();
     }
 
     update(key: K, value: V): void {
+        // check if exists
+        let node = this.lookUp.get(key);
+        if (!node) {
+            // if not insert
+            // check capcity and evict if over
+            node = createNode(value);
+            this.length++;
+            this.prepend(node);
+            this.trimCache();
+            this.lookUp.set(key, node);
+            this.reverseLookUp.set(node, key);
+        } else {
+            //if it does update it to the front and update the value
+            this.detach(node);
+            this.prepend(node);
+            node.value = value;
+        }
+    }
 
-}
     get(key: K): V | undefined {
+        // check if exists
+        const node = this.lookUp.get(key);
+        if (!node) {
+            return undefined;
+        }
+        // update the value and move it to the front
+        this.detach(node);
+        this.prepend(node);
+        // return the value || undefined
+        return node.value;
+    }
 
-}
+    private detach(node: Node<V>): void {
+        if (node.prev) {
+            node.prev.next = node.next;
+        }
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+        // if (this.length === 1) {
+        //     this.tail = this.head = undefined
+        // }
+
+        if (this.head === node) {
+            this.head = this.head.next;
+        }
+        if (this.tail === node) {
+            this.tail = this.tail.prev;
+        }
+
+        node.next = undefined;
+        node.prev = undefined;
+    }
+
+    private prepend(node: Node<V>): void {
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+
+    private trimCache(): void {
+        if (this.length <= this.capacity) {
+            return;
+        }
+
+        const tail = this.tail as Node<V>;
+        this.detach(this.tail as Node<V>);
+
+        const key = this.reverseLookUp.get(tail) as K;
+        this.lookUp.delete(key);
+        this.reverseLookUp.delete(tail);
+        this.length--;
+    }
 }
